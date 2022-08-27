8ea932fdbb65b57177f6213ba5f9942c7ab2eca3
daichi0918 <kayasugadaichi@icloud.com>
2022-08-27 10:21:06 +0900

    cors設定完了

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,24 +1,16 @@
+import axios from 'axios';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const onClickFetchUserData = () => {
+    axios.get("http://localhost:3002/api/v1/users").then((res) => {
+      console.log(res)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickFetchUserData}>ユーザーデータ取得</button>
     </div>
   );
 }
