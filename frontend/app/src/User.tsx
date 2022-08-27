c48f9f3624a894766ffa6314ddbe673143fb9541
daichi0918 <kayasugadaichi@icloud.com>
2022-08-27 13:42:42 +0900

    props(users)に型定義を実装

--- frontend/app/src/User.tsx
+++ frontend/app/src/User.tsx
@@ -1,4 +1,16 @@
-export const User = (props) => {
+type UserType = {
+  // id: number;
+  name: string;
+  // email: string;
+  // encrypted_password: string;
+  // gender: string;
+  // image: string;
+  // memo: string;
+  // created_at: string;
+  // updated_at: string;
+}
+
+export const User = (props: UserType) => {
   const { name } = props;
   return <p>{`ユーザー名：${name}`}</p>
 }
