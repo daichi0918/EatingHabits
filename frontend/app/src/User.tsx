e45a35d1a5ff077ddbac48e2bc233b4403fca39b
daichi0918 <kayasugadaichi@icloud.com>
2022-08-28 22:12:21 +0900

    memory不足の解消

--- frontend/app/src/User.tsx
+++ frontend/app/src/User.tsx
@@ -1,4 +1,4 @@
-// import { FC } from "react";
+import { FC } from "react";
 // import { UserProfile } from "./types/userProfile";
 
 // export const User: FC<Pick<UserProfile, "name" | "email">> = (props) => {
