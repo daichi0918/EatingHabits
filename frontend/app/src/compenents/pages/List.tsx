1741215c09bd8953e9b11e99cd8ee5f15d06ae4c
daichi0918 <kayasugadaichi@icloud.com>
2022-09-04 11:19:16 +0900

    Listをテーブル表示した

--- frontend/app/src/compenents/pages/List.tsx
+++ frontend/app/src/compenents/pages/List.tsx
@@ -1,4 +1,15 @@
-import { Spinner, Center, Wrap, WrapItem } from "@chakra-ui/react";
+import {
+  Spinner, Center, Wrap, WrapItem, Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons"
 import { FC, memo, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useAllLists } from "../../hooks/useAllLists";
@@ -21,15 +32,33 @@ export const List: FC = memo(() => {
         </Center>
 
       ) : (
-          <Wrap p={{ base: 4, md: 10 }}>
-            {lists.map((list) => (
-              <WrapItem mx="auto">
-                {list.name}
-              </WrapItem>
+          // <Wrap p={{ base: 4, md: 10 }}>
+          //   {lists.map((list) => (
+          //     <WrapItem mx="auto">
+          //       {list.name}
+          //     </WrapItem>
 
-            ))}
+          //   ))}
 
-          </Wrap>
+          // </Wrap>
+          <TableContainer>
+            <Table variant='simple'>
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Delete</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {lists.map((list) => (
+                  <Tr>
+                    <Td>{list.name}</Td>
+                    <Td><DeleteIcon /></Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
         )
       }
 
