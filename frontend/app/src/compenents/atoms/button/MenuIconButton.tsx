0808a54182e34074b4f4a9ea2f0d0ed91008f852
daichi0918 <kayasugadaichi@icloud.com>
2022-09-11 15:49:17 +0900

    hamburgerIcon差し替え

--- frontend/app/src/compenents/atoms/button/MenuIconButton.tsx
+++ frontend/app/src/compenents/atoms/button/MenuIconButton.tsx
@@ -1,4 +1,6 @@
 // import { HamburgerIcon } from "@chakra-ui/icons";
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box, IconButton } from '@mui/material';
 import { FC, memo } from "react";
 
 type Props = {
@@ -9,6 +11,17 @@ export const MenuIconButton: FC<Props> = memo((props) => {
   const { onOpen } = props;
   return (
     // <HamburgerIcon area-label="メニューボタン" w={6} h={6} display={{ base: "block", md: "none" }} onClick={onOpen} />
-    <div>=</div>
+    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+      <IconButton
+        onClick={onOpen}
+        size="large"
+        aria-label="メニューボタン"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        color="inherit"
+      >
+        <MenuIcon />
+      </IconButton>
+    </Box>
   )
 })
