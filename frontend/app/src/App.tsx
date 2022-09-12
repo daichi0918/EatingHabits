47c891cef5c55d2cdbe3fd768af3e316a37c79ac
daichi0918 <kayasugadaichi@icloud.com>
2022-09-12 23:14:46 +0900

    「5. ログイン状態と非ログイン状態でページを切り分ける」完了

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,18 +1,77 @@
 // import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { FC, createContext, useState, useEffect } from 'react';
+import { BrowserRouter, Navigate, Route } from 'react-router-dom';
 import { ThemeProvider } from "@mui/material";
-import theme from "./theme/theme";
-import { Router } from './router/Router';
 
-function App() {
+import theme from "./theme/theme";
+import { getCurrentUser } from "../src/apis/auth";
+import { Router } from './router/Router';
+import { UserType } from '../src/types/api/usertype';
+import { Home } from "../src/compenents/pages/Home";
+
+
+export const AuthContext = createContext({} as {
+  loading: boolean
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  isSignedIn: boolean
+  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+  currentUser: UserType | undefined
+  setCurrentUser: React.Dispatch<React.SetStateAction<UserType | undefined>>
+})
+
+const App: FC = () => {
+
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
+  const [currentUser, setCurrentUser] = useState<UserType | undefined>()
+
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.data)
+
+        console.log(res?.data.data)
+      } else {
+        console.log("No current user")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
+  const Private = ({ children }: { children: React.ReactElement }) => {
+    if (!loading) {
+      if (isSignedIn) {
+        return children
+      } else {
+        return <Navigate to="/signin" />
+      }
+    } else {
+      return <></>
+    }
+  }
 
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter >
-    </ThemeProvider>
+    <AuthContext.Provider value={{ loading, setLoading, isSignedIn, setIsSignedIn, currentUser, setCurrentUser }}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+          <Private>
+            <Route path="/" element={<Home />} />
+          </Private>
+        </BrowserRouter >
+      </ThemeProvider>
+    </AuthContext.Provider>
+
     // <ChakraProvider theme={theme}>
 
     // </ChakraProvider>
--- frontend/app/src/router/Router.tsx
+++ frontend/app/src/router/Router.tsx
@@ -1,5 +1,5 @@
 import { FC, memo } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { Home } from "../compenents/pages/Home";
 import { Login } from "../compenents/pages/Login";
@@ -8,12 +8,31 @@ import { UserManagement } from "../compenents/pages/UserManagement";
 import { Lists } from "../compenents/pages/Lists"
 import { HeaderLayout } from "../compenents/templates/HeaderLayout";
 import { homeRoutes } from "./HomeRoutes";
+import { SignUp } from "../compenents/pages/SignUp";
+import { SignIn } from "../compenents/pages/SignIn";
 
-export const Router: FC = memo(() => {
+export const Router: FC = memo((props: any) => {
+  // const { loading, isSignedIn } = props;
+  // const Private = ({ children }: { children: React.ReactElement }) => {
+  //   if (!loading) {
+  //     if (isSignedIn) {
+  //       return children
+  //     } else {
+  //       return <Navigate to="/signin" />
+  //     }
+  //   } else {
+  //     return <></>
+  //   }
+  // }
   return (
     <Routes>
       <Route path="" element={<Login />} />
       <Route path="home" element={<Home />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="signin" element={<SignIn />} />
+      {/* <Private>
+        <Route path="/" element={<Home />} />
+      </Private> */}
       <Route path="home/user_management" element={<UserManagement />} />
       <Route path="home/user_management/:userId/list" element={<Lists />} />
       <Route path="*" element={<Page404 />} />
