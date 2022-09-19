9e104077b754efad04f6b2125b00fa1e9515ac4c
daichi0918 <kayasugadaichi@icloud.com>
2022-09-19 19:16:19 +0900

    簡易的なBox作成完了

--- frontend/app/src/compenents/pages/Home.tsx
+++ frontend/app/src/compenents/pages/Home.tsx
@@ -1,7 +1,8 @@
-import { Button } from "@mui/material";
 import axios from "axios";
 import { FC, memo, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import Box from '@mui/material/Box';
 
 import { AuthContext } from "../../App";
 import { HomeHeaderLayout } from "../templates/HomeHeaderLayout";
@@ -16,13 +17,30 @@ export const Home: FC = memo(() => {
   //   }));
   return (
     <HomeHeaderLayout>
-      <Button
-        variant="outlined"
-        component={Link}
-        to={`/home/list`}
+
+      <Box
+        sx={{
+          width: 1200,
+          height: 600,
+          backgroundColor: 'white',
+          border: '1',
+          borderColor: 'grey.500',
+          mx: 'auto',
+          mt: 4,
+          // '&:hover': {
+          //   backgroundColor: 'primary.main',
+          //   opacity: [0.9, 0.8, 0.7],
+          // },
+        }}
       >
-        List
+        <Button
+          variant="outlined"
+          component={Link}
+          to={`/home/list`}
+        >
+          List
       </Button>
+      </Box>
     </HomeHeaderLayout>
   )
 })
