34a5866d36e19be93069e114c6246c342f374e1f
daichi0918 <kayasugadaichi@icloud.com>
2022-09-19 09:13:10 +0900

    signupUI仮完了

--- frontend/app/src/compenents/pages/SignUp.tsx
+++ frontend/app/src/compenents/pages/SignUp.tsx
@@ -1,6 +1,19 @@
 import Cookies from "js-cookie";
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import { signUp } from "../../apis/auth";
 import { AuthContext } from "../../App";
 import { SignUpParams } from "../../types/api/auth";
@@ -42,16 +55,18 @@ export const SignUp = () => {
 
         console.log("Signed in successfully!")
       } else {
-        alert("eleseエラー")
+        alert("elseエラー")
       }
     } catch (err) {
       console.log(err)
       alert("catchエラー")
     }
   }
+
+  const theme = createTheme();
   return (
     <>
-      <h1>サインアップページです</h1>
+      {/* <h1>サインアップページです</h1>
       <form>
         <div>
           <label htmlFor="name">名前</label>
@@ -105,7 +120,126 @@ export const SignUp = () => {
           Submit
         </button>
       </form>
-      <Link to="/signin">サインインへ</Link>
+      <Link to="/signin">サインインへ</Link> */}
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+          </Typography>
+            <Box component="form" noValidate sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                {/* <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid> */}
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="Name"
+                    label="Name"
+                    name="name"
+                    autoComplete="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password_confirmation"
+                    label="Password Confirmation"
+                    type="password"
+                    id="password_confirmation"
+                    autoComplete="new-password_confirmation"
+                    value={passwordConfirmation}
+                    onChange={(e) => setPasswordConfirmation(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                onClick={(e) => handleSignUpSubmit(e)}
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+            </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link to="/signin">
+                    Already have an account? Sign in
+                </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+
+        </Container>
+      </ThemeProvider>
+
     </>
   );
 };
