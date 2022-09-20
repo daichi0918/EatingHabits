ff1d58a300c24dd22550a296cc754cab0b75d453
daichi0918 <kayasugadaichi@icloud.com>
2022-09-20 16:17:58 +0900

    home画面のCard作成仮完了

--- frontend/app/src/compenents/pages/Home.tsx
+++ frontend/app/src/compenents/pages/Home.tsx
@@ -1,8 +1,16 @@
 import axios from "axios";
 import { FC, memo, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { grey } from '@mui/material/colors';
+import { CardActionArea } from '@mui/material';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Grid';
 
 import { AuthContext } from "../../App";
 import { HomeHeaderLayout } from "../templates/HomeHeaderLayout";
@@ -15,6 +23,9 @@ export const Home: FC = memo(() => {
   //     name: user.name,
   //     email: user.email
   //   }));
+  const navigate = useNavigate();
+
+  const onClickList = () => navigate("list")
   return (
     <HomeHeaderLayout>
 
@@ -24,7 +35,7 @@ export const Home: FC = memo(() => {
           height: 600,
           backgroundColor: 'white',
           border: '1',
-          borderColor: 'grey.500',
+          borderColor: grey[500],
           mx: 'auto',
           mt: 4,
           // '&:hover': {
@@ -33,13 +44,37 @@ export const Home: FC = memo(() => {
           // },
         }}
       >
-        <Button
-          variant="outlined"
-          component={Link}
-          to={`/home/list`}
+        <Card
+          sx={{
+            width: 280,
+            height: 150,
+            m: 2,
+            backgroundColor: grey[100],
+            '&:hover': {
+              opacity: [0.9, 0.8, 0.7],
+              cursor: 'pointer'
+            },
+          }}
+          onClick={onClickList}
         >
-          List
-      </Button>
+          <CardContent>
+            <Grid container>
+              <Grid item xs={6} md={3}>
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                  <FormatListBulletedIcon />
+                </Avatar>
+              </Grid>
+              <Grid item xs={6} md={9}>
+                <Typography variant="h6" component="div" sx={{ m: 1.5 }}>
+                  買い物リスト
+                </Typography>
+              </Grid>
+            </Grid>
+            <Typography variant="body2" align="center" sx={{ m: 1.5 }}>
+              買いたいものをリストアップする
+            </Typography>
+          </CardContent>
+        </Card>
       </Box>
     </HomeHeaderLayout>
   )
