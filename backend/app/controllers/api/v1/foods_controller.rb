06f85047cb8cc5e347d4aef80d8a534fda31666f
daichi0918 <kayasugadaichi@icloud.com>
2022-09-21 18:44:20 +0900

    controllerのupdate以外のメソッド実装完了

---
+++ backend/app/controllers/api/v1/foods_controller.rb
@@ -0,0 +1,40 @@
+module Api
+  module V1
+    class FoodsController < ApplicationController
+      before_action :set_food, only: %i[destroy]
+
+      def index
+        user = User.find(params[:user_id])
+        foods = user.foods
+
+        render json: {
+          foods: foods
+        }, status: :ok
+      end
+
+      def create
+        food = Food.new(food_params)
+        if food.save
+          render json: { status: 'SUCCESS', data: food }
+        else
+          render json: { status: 'ERROR', data: food.errors }
+        end
+      end
+
+      def destroy
+        @food.destroy
+        render json: { status: 'SUCCESS', message: 'Deleted the food', data: @food }
+      end
+
+      private
+
+      def set_food
+        @food = Food.find(params[:id])
+      end
+
+      def food_params
+        params.permit(:classification_id, :name, :quantity, :expired_at, :notified_at, :image, :memo).merge(user_id: params[:user_id])
+      end
+    end
+  end
+end
