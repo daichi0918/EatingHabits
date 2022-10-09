0e4023dd25da7f4d7e67dce92afb827920a33b5c
daichi0918 <kayasugadaichi@icloud.com>
2022-10-09 11:22:05 +0900

    update:posts_controller.rb,order by created_at desc

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -4,7 +4,7 @@ module Api
       before_action :set_post, only: %i[edit update destroy]
 
       def index
-        posts = Post.joins(:user).select('posts.*, users.name as userName,users.image as userImage')
+        posts = Post.joins(:user).select('posts.*, users.name as userName,users.image as userImage').order(created_at: "DESC")
 
         render json: {
           posts: posts
