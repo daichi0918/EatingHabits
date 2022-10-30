c975ae90278c041f65b0c5c691d398eaf633688a
daichi0918 <kayasugadaichi@icloud.com>
2022-10-30 18:12:41 +0900

    add-column:birthday

---
+++ backend/db/migrate/20221030091030_add_birth_day_to_users.rb
@@ -0,0 +1,5 @@
+class AddBirthDayToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :birthday, :date
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_094804) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_30_091030) do
   create_table "bookmarks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -126,6 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_23_094804) do
     t.datetime "updated_at", null: false
     t.integer "gender", default: 0, null: false
     t.string "memo"
+    t.date "birthday"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
