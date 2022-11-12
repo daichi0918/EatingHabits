3394ea8c5339058d214a90b9ef33c629a11ee96d
daichi0918 <kayasugadaichi@icloud.com>
2022-11-12 18:19:49 +0900

    commit

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -2,7 +2,9 @@ require 'dotenv'
 Dotenv.load
 
 CarrierWave.configure do |config|
-  config.asset_host = ENV['HOST']
+  # config.asset_host = ENV['HOST']
+  # config.asset_host = 'http://localhost:3002'
+  config.asset_host = 'http://160.251.98.255:3002'
   config.storage = :file
   config.cache_storage = :file
 end
